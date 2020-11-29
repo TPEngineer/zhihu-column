c8e75ab79190d7b9770cbee09c2858c2dcf84406
HashidaHaru <nvhashida@icloud.com>
2020-11-29 09:20:45 +0800

    优化首页UI

---
+++ src/components/Layout.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-layout
+    id="components-layout-demo-top"
+    class="layout"
+    :style="{ height: '100vh' }"
+  >
+    <a-layout-header style="background: white">
+      <div :style="{ float: 'left' }">知乎专栏</div>
+      <div :style="{ lineHeight: '64px', float: 'right' }">
+        <a-button type="primary">登陆 </a-button>
+        <a-button type="primary" :style="{ marginLeft: '12px' }"
+          >注册
+        </a-button>
+      </div>
+    </a-layout-header>
+    <a-layout-content :style="{ padding: '50px' }">
+      <div :style="{ background: '#fff', padding: '24px' }">
+        <router-view />
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Ant Design ©2018 Created by Ant UED
+    </a-layout-footer>
+  </a-layout>
+</template>
+<script>
+export default {
+  data() {
+    return { selectedKeys: ["2"] };
+  }
+};
+</script>
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <a-row type="flex" justify="center">
+    <img src="../assets/callout.svg" alt="callout" width="250" />
+  </a-row>
+  <h1 style="text-align: center">随心写作，自由表达</h1>
+  <a-row type="flex" justify="center">
+    <a-button type="primary">开始写文章</a-button>
+  </a-row>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      selectedKeys: ["2"]
+    };
   }
 };
 </script>
+<style>
+#components-layout-demo-top .logo {
+  width: 120px;
+  height: 31px;
+  /* background: rgba(255, 255, 255, 0.2); */
+  margin: 16px 24px 16px 0;
+  float: left;
+}
+</style>
