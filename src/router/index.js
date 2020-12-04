db06394384ab6bd492f4fa7819ef36e734df7669
xinxin <nvhashida@icloud.com>
2020-12-04 10:49:47 +0800

    设计专栏页布局

--- src/router/index.js
+++ src/router/index.js
@@ -14,13 +14,13 @@ const routes = [
         component: Home
       },
       {
-        path: "/about",
-        name: "about",
+        path: "/column",
+        name: "column",
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () =>
-          import(/* webpackChunkName: "about" */ "../views/About.vue")
+          import(/* webpackChunkName: "about" */ "../views/Column.vue")
       },
       {
         path: "/user/detail",
---
+++ src/views/Column.vue
@@ -0,0 +1,38 @@
+<template>
+  <a-row :gutter="16">
+    <a-col :span="6">
+      <a-card title="Card title" hoverable>
+        <p>card content</p>
+      </a-card>
+    </a-col>
+    <a-col :span="6">
+      <a-card title="Card title" hoverable>
+        <p>card content</p>
+      </a-card>
+    </a-col>
+    <a-col :span="6">
+      <a-card hoverable>
+        <template #cover>
+          <img
+            alt="example"
+            src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3895225068,1041998716&fm=26&gp=0.jpg"
+          />
+        </template>
+
+        <a-card-meta title="Card title" description="This is the description">
+          <template #avatar>
+            <a-avatar
+              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+            />
+          </template>
+        </a-card-meta>
+      </a-card>
+    </a-col>
+  </a-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
