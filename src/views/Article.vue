65e0ac306cce36a92971268e38b5dca8514aa857
xinxin <nvhashida@icloud.com>
2020-12-08 08:48:08 +0800

    完成文章详情页

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,12 +1,46 @@
 <template>
-  <h1>detail</h1>
+  <a-row type="flex" justify="center">
+    <a-col :md="10">
+      <img alt="logo" :src="article.header_image" />
+      <h1
+        :style="{
+          fontSize: '24px',
+          lineHeight: '1.22',
+          margin: '24px 0'
+        }"
+      >
+        {{ article.title }}
+      </h1>
+      <div v-html="article.content"></div>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
+import { useRoute } from "vue-router";
+import service from "@/utils/request";
+import { onMounted, ref } from "vue";
 export default {
-
-}
+  setup() {
+    const article = ref({});
+    const getArticleDetail = async () => {
+      const route = useRoute();
+      const { article_id } = route.query;
+      const res = await service.post("/article/detail", {
+        article_id: parseInt(article_id, 10)
+      });
+      console.log(res);
+      article.value = res;
+    };
+    // fetch the user information when params change
+    onMounted(() => {
+      getArticleDetail();
+    });
+    return {
+      article
+    };
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
